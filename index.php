75af54f25e80ef6a56a8eee19285d839a5db6724
Payton Garrard <jpgarrard@mail.lipscomb.edu>
2014-10-28 14:02:20 -0500

    Add Google Analytics
    Tracking of site traffic

--- index.php
+++ index.php
@@ -95,7 +95,16 @@ $t->title('Peugeot Center'); // Set the title of the page
 		float: none;
 	}
 }
-</style>
+</style><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50741714-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <script>
 $(document).ready(function() {
 var slides = $('#slideshow a').remove().each(function() {
