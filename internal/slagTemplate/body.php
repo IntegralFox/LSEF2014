81a74af6a83209311e2af2973fb56bb3ac0ba25d
IntegralFox <michael.welborn0775@gmail.com>
2014-11-02 13:30:08 -0600

    Add files link to navbar

--- internal/slagTemplate/body.php
+++ internal/slagTemplate/body.php
@@ -7,6 +7,7 @@
 			<a href="./"><img src="img/ic_home_black_48dp.png"> Home</a>
 			<a href="about.php"><img src="img/ic_live_help_black_48dp.png"> About Us</a>
 			<a href="contact.php"><img src="img/ic_email_black_48dp.png"> Contact Us</a>
+			<a href="files.php"><img src="img/ic_attachment_black_48dp.png"> Files</a>
 			<a href="donate.php"><img src="img/ic_local_atm_black_48dp.png"> Donate</a>
 		</nav>
 		<main>
@@ -16,6 +17,7 @@
 			<a href="./">Home</a>
 			<a href="about.php">About Us</a>
 			<a href="contact.php">Contact Us</a>
+			<a href="files.php">Files</a>
 			<a href="donate.php">Donate</a>
 		</footer>
 	</body>
