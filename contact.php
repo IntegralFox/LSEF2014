448f5ddb4effaf8300b614fbef7b7e32e9433709
IntegralFox <michael.welborn0775@gmail.com>
2014-11-17 15:34:08 -0600

    Add contact form and mailto URI

--- contact.php
+++ contact.php
@@ -7,7 +7,11 @@ $t->title('Peugeot Center'); // Set the title of the page
 
 ?>
 <script>
+	var email = 'csimmons@liscomb.edu';
 	$(document).ready(function() {
+		$('#send').click(function() {
+			window.open('mailto:' + email + '?subject=' + encodeURI($('#subject').val()) + '&body=' + encodeURI($('#body').val()));
+		});
 	});
 </script>
 <?php
@@ -15,6 +19,17 @@ $t->title('Peugeot Center'); // Set the title of the page
 $t->scrapeMeta();
 
 ?>
+<h1>Emails to Us</h1>
+<form>
+	<input type="text" id="subject" placeholder="Enter a Subject"><br>
+	<textarea id="body" placeholder="Enter a Message"></textarea><br>
+	<input type="button" id="send" value="Send">
+</form>
+<h1>Newsletters to You</h1>
+<form action="contact.php" method="POST">
+	<input type="email" name="email" placeholder="Enter Your Email">
+	<input type="submit" value="Sign Up">
+</form>
 <?php
 
 $t->scrapeContent();
